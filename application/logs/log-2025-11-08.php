bc50f2041369e039834667e84957f7a09eae0588
Fauzi Hartanto <155997483+FauziHrtnt@users.noreply.github.com>
2025-11-09 02:20:58 +0700

    Upload ulang dari awal ke branch new_inventory

---
+++ application/logs/log-2025-11-08.php
@@ -0,0 +1,35 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-11-08 04:21:32 --> 404 Page Not Found: /index
+ERROR - 2025-11-08 04:33:44 --> Severity: Warning --> include(_js.php): failed to open stream: No such file or directory C:\laragon\www\tmfw-main\application\modules\asset\views\asset\index.php 1
+ERROR - 2025-11-08 04:33:44 --> Severity: Warning --> include(): Failed opening '_js.php' for inclusion (include_path='.;C:/laragon/etc/php/pear') C:\laragon\www\tmfw-main\application\modules\asset\views\asset\index.php 1
+ERROR - 2025-11-08 04:34:39 --> Query error: Table 'tmfw_inventory_management_system.trx_asset_masuk' doesn't exist - Invalid query: SELECT *
+FROM `trx_asset_masuk`
+WHERE `id` IS NULL
+ERROR - 2025-11-08 20:02:03 --> Query error: Table 'tmfw_inventory_management_system.trx_asset_masuk' doesn't exist - Invalid query: SELECT *
+FROM `trx_asset_masuk`
+WHERE `id` IS NULL
+ERROR - 2025-11-08 21:10:32 --> Severity: error --> Exception: Call to a member function language() on null C:\laragon\www\tmfw-main\system\libraries\Pagination.php 333
+ERROR - 2025-11-08 21:11:33 --> Severity: error --> Exception: Call to a member function language() on null C:\laragon\www\tmfw-main\system\libraries\Pagination.php 333
+ERROR - 2025-11-08 21:14:10 --> 404 Page Not Found: ../modules/asset/controllers/Api_asset/tes
+ERROR - 2025-11-08 21:14:13 --> 404 Page Not Found: ../modules/asset/controllers/Api_asset/tes
+ERROR - 2025-11-08 21:14:32 --> 404 Page Not Found: ../modules/asset/controllers/Api_asset/tes
+ERROR - 2025-11-08 21:14:34 --> 404 Page Not Found: ../modules/asset/controllers/Api_asset/tes
+ERROR - 2025-11-08 21:14:34 --> 404 Page Not Found: ../modules/asset/controllers/Api_asset/tes
+ERROR - 2025-11-08 21:14:35 --> 404 Page Not Found: ../modules/asset/controllers/Api_asset/tes
+ERROR - 2025-11-08 21:14:35 --> 404 Page Not Found: ../modules/asset/controllers/Api_asset/tes
+ERROR - 2025-11-08 22:36:55 --> Query error: Duplicate entry '02' for key 'app_nav.PRIMARY' - Invalid query: UPDATE `app_nav` SET `nav_parent` = '03', `nav_id` = '02', `nav_nm` = 'Asset Keluar', `nav_url` = 'asset/asset_keluar', `icon` = NULL, `active_st` = '1', `updated_at` = '2025-11-08 22:36:55', `updated_by` = 'PEGAWAI TESTER'
+WHERE `nav_id` = '03.02'
+ERROR - 2025-11-08 22:37:52 --> Query error: Unknown column 'pegawai_id' in 'where clause' - Invalid query: SELECT *
+FROM `app_permission`
+WHERE `pegawai_id` = '000000000000'
+AND `nav_id` = '03.02'
+ERROR - 2025-11-08 22:38:02 --> Query error: Unknown column 'pegawai_id' in 'where clause' - Invalid query: SELECT *
+FROM `app_permission`
+WHERE `pegawai_id` = '000000000000'
+AND `nav_id` = '03.02'
+ERROR - 2025-11-08 22:38:09 --> 404 Page Not Found: ../modules/master/controllers//index
+ERROR - 2025-11-08 22:39:39 --> 404 Page Not Found: ../modules/asset/controllers//index
+ERROR - 2025-11-08 22:54:33 --> 404 Page Not Found: /index
+ERROR - 2025-11-08 22:54:36 --> 404 Page Not Found: /index
+ERROR - 2025-11-08 23:51:19 --> 404 Page Not Found: ../modules/asset/controllers//index
---
+++ application/logs/log-2025-11-09.php
@@ -0,0 +1,57 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-11-09 00:01:28 --> 404 Page Not Found: ../modules/master/controllers//index
+ERROR - 2025-11-09 00:05:27 --> Query error: Table 'tmfw_inventory_management_system.mst_kategori' doesn't exist - Invalid query: SELECT a.*, k.kategori_nama, s.satuan_nama 
+                  FROM mst_asset a 
+                  LEFT JOIN mst_kategori k ON a.kategori_id = k.kategori_id 
+                  LEFT JOIN mst_satuan s ON a.satuan_id = s.satuan_id WHERE 1 = 1  AND a.deleted_st='0' AND (LOWER(a.asset_kode) LIKE '%%' OR LOWER(a.asset_nama) LIKE '%%' OR LOWER(k.kategori_nama) LIKE '%%')  ORDER BY asset_id asc LIMIT 10 OFFSET 0
+ERROR - 2025-11-09 00:05:31 --> Query error: Table 'tmfw_inventory_management_system.mst_kategori' doesn't exist - Invalid query: SELECT *
+FROM `mst_kategori`
+WHERE `deleted_st` = 0
+ERROR - 2025-11-09 00:05:41 --> Query error: Table 'tmfw_inventory_management_system.mst_kategori' doesn't exist - Invalid query: SELECT a.*, k.kategori_nama, s.satuan_nama 
+                  FROM mst_asset a 
+                  LEFT JOIN mst_kategori k ON a.kategori_id = k.kategori_id 
+                  LEFT JOIN mst_satuan s ON a.satuan_id = s.satuan_id WHERE 1 = 1  AND a.deleted_st='0' AND (LOWER(a.asset_kode) LIKE '%%' OR LOWER(a.asset_nama) LIKE '%%' OR LOWER(k.kategori_nama) LIKE '%%')  ORDER BY asset_id asc LIMIT 10 OFFSET 0
+ERROR - 2025-11-09 00:07:25 --> 404 Page Not Found: /index
+ERROR - 2025-11-09 00:07:48 --> Query error: Table 'tmfw_inventory_management_system.mst_kategori' doesn't exist - Invalid query: SELECT a.*, k.kategori_nama, s.satuan_nama 
+                  FROM mst_asset a 
+                  LEFT JOIN mst_kategori k ON a.kategori_id = k.kategori_id 
+                  LEFT JOIN mst_satuan s ON a.satuan_id = s.satuan_id WHERE 1 = 1  AND a.deleted_st='0' AND (LOWER(a.asset_kode) LIKE '%%' OR LOWER(a.asset_nama) LIKE '%%' OR LOWER(k.kategori_nama) LIKE '%%')  ORDER BY asset_id asc LIMIT 10 OFFSET 0
+ERROR - 2025-11-09 00:21:03 --> Query error: Table 'tmfw_inventory_management_system.mst_kategori' doesn't exist - Invalid query: SELECT a.*, k.kategori_nama, s.satuan_nama 
+                  FROM mst_asset a 
+                  LEFT JOIN mst_kategori k ON a.kategori_id = k.kategori_id 
+                  LEFT JOIN mst_satuan s ON a.satuan_id = s.satuan_id WHERE 1 = 1  AND a.deleted_st='0' AND (LOWER(a.asset_kode) LIKE '%%' OR LOWER(a.asset_nama) LIKE '%%' OR LOWER(k.kategori_nama) LIKE '%%')  ORDER BY asset_id asc LIMIT 10 OFFSET 0
+ERROR - 2025-11-09 00:21:10 --> Query error: Table 'tmfw_inventory_management_system.mst_kategori' doesn't exist - Invalid query: SELECT a.*, k.kategori_nama, s.satuan_nama 
+                  FROM mst_asset a 
+                  LEFT JOIN mst_kategori k ON a.kategori_id = k.kategori_id 
+                  LEFT JOIN mst_satuan s ON a.satuan_id = s.satuan_id WHERE 1 = 1  AND a.deleted_st='0' AND (LOWER(a.asset_kode) LIKE '%%' OR LOWER(a.asset_nama) LIKE '%%' OR LOWER(k.kategori_nama) LIKE '%%')  ORDER BY asset_id asc LIMIT 10 OFFSET 0
+ERROR - 2025-11-09 00:21:46 --> Query error: Table 'tmfw_inventory_management_system.mst_kategori' doesn't exist - Invalid query: SELECT a.*, k.kategori_nama, s.satuan_nama 
+                  FROM mst_asset a 
+                  LEFT JOIN mst_kategori k ON a.kategori_id = k.kategori_id 
+                  LEFT JOIN mst_satuan s ON a.satuan_id = s.satuan_id WHERE 1 = 1  AND a.deleted_st='0' AND (LOWER(a.asset_kode) LIKE '%%' OR LOWER(a.asset_nama) LIKE '%%' OR LOWER(k.kategori_nama) LIKE '%%')  ORDER BY asset_kode asc LIMIT 500 OFFSET 0
+ERROR - 2025-11-09 00:21:49 --> Query error: Table 'tmfw_inventory_management_system.mst_kategori' doesn't exist - Invalid query: SELECT *
+FROM `mst_kategori`
+WHERE `deleted_st` = 0
+ERROR - 2025-11-09 00:21:55 --> Query error: Table 'tmfw_inventory_management_system.mst_kategori' doesn't exist - Invalid query: SELECT a.*, k.kategori_nama, s.satuan_nama 
+                  FROM mst_asset a 
+                  LEFT JOIN mst_kategori k ON a.kategori_id = k.kategori_id 
+                  LEFT JOIN mst_satuan s ON a.satuan_id = s.satuan_id WHERE 1 = 1  AND a.deleted_st='0' AND (LOWER(a.asset_kode) LIKE '%%' OR LOWER(a.asset_nama) LIKE '%%' OR LOWER(k.kategori_nama) LIKE '%%')  ORDER BY asset_kode asc LIMIT 500 OFFSET 0
+ERROR - 2025-11-09 00:24:15 --> Query error: Table 'tmfw_inventory_management_system.mst_kategori' doesn't exist - Invalid query: SELECT a.*, k.kategori_nama, s.satuan_nama 
+                  FROM mst_asset a 
+                  LEFT JOIN mst_kategori k ON a.kategori_id = k.kategori_id 
+                  LEFT JOIN mst_satuan s ON a.satuan_id = s.satuan_id WHERE 1 = 1  AND a.deleted_st='0' AND (LOWER(a.asset_kode) LIKE '%%' OR LOWER(a.asset_nama) LIKE '%%' OR LOWER(k.kategori_nama) LIKE '%%')  ORDER BY asset_kode asc LIMIT 500 OFFSET 0
+ERROR - 2025-11-09 00:28:01 --> 404 Page Not Found: /index
+ERROR - 2025-11-09 00:28:50 --> Query error: Table 'tmfw_inventory_management_system.mst_kategori' doesn't exist - Invalid query: SELECT *
+FROM `mst_kategori`
+WHERE `deleted_st` = 0
+ERROR - 2025-11-09 00:29:30 --> 404 Page Not Found: /index
+ERROR - 2025-11-09 00:29:36 --> 404 Page Not Found: /index
+ERROR - 2025-11-09 00:31:38 --> 404 Page Not Found: /index
+ERROR - 2025-11-09 00:34:07 --> Query error: Table 'tmfw_inventory_management_system.mst_kategori' doesn't exist - Invalid query: SELECT a.*, k.kategori_nama, s.satuan_nama 
+                FROM mst_asset a 
+                LEFT JOIN mst_kategori k ON a.kategori_id = k.kategori_id 
+                LEFT JOIN mst_satuan s ON a.satuan_id = s.satuan_id WHERE 1 = 1  AND a.deleted_st='0' AND (LOWER(a.asset_kode) LIKE '%%' OR LOWER(a.asset_nama) LIKE '%%' OR LOWER(k.kategori_nama) LIKE '%%')  ORDER BY asset_kode asc LIMIT 500 OFFSET 0
+ERROR - 2025-11-09 00:35:01 --> Query error: Table 'tmfw_inventory_management_system.mst_kategori' doesn't exist - Invalid query: SELECT a.*, k.kategori_nama, s.satuan_nama 
+                FROM mst_asset a 
+                LEFT JOIN mst_kategori k ON a.kategori_id = k.kategori_id 
+                LEFT JOIN mst_satuan s ON a.satuan_id = s.satuan_id WHERE 1 = 1  AND a.deleted_st='0' AND (LOWER(a.asset_kode) LIKE '%%' OR LOWER(a.asset_nama) LIKE '%%' OR LOWER(k.kategori_nama) LIKE '%%')  ORDER BY asset_kode asc LIMIT 500 OFFSET 0
