f223cff50258641943b4cd89f5258ea5e8b3b223
awangihti <studentawangihti@gmail.com>
2025-12-16 09:06:30 +0700

    update file

---
+++ application/logs/log-2025-12-10.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-12-10 11:49:26 --> 404 Page Not Found: ../modules/api/controllers//index
+ERROR - 2025-12-10 12:06:17 --> Severity: error --> Exception: Call to a member function language() on null C:\laragon\www\Project_Magang\system\libraries\Pagination.php 333
+ERROR - 2025-12-10 12:06:18 --> Severity: error --> Exception: Call to a member function language() on null C:\laragon\www\Project_Magang\system\libraries\Pagination.php 333
+ERROR - 2025-12-10 12:06:18 --> Severity: error --> Exception: Call to a member function language() on null C:\laragon\www\Project_Magang\system\libraries\Pagination.php 333
+ERROR - 2025-12-10 12:06:19 --> Severity: error --> Exception: Call to a member function language() on null C:\laragon\www\Project_Magang\system\libraries\Pagination.php 333
